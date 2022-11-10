9e3052a7cf0a514ab5597fd0b0ef4bc2e6716140
Arik Alexandrov <arikxl@gmail.com>
2022-11-11 00:06:34 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -16,6 +16,12 @@ const handleSubmit= (e) => {
         setTimeout(() => {
             hideSpinner()
             generateQrCode(url, size);
+
+            setTimeout(() => {
+                const saveUrl = qrElement.querySelector('img').src;
+                createSaveBtn(saveUrl);
+            },50)
+
         }, 2000)
     }
 }
@@ -38,6 +44,20 @@ const hideSpinner = () => {
 
 const clearUI = () => {
     qrElement.innerHTML='';
+    const saveBtn = document.getElementById('save-link')
+    if(saveBtn) {
+        saveBtn.remove()
+    }
+}
+
+const createSaveBtn = (saveURL) => {
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList = 'rounded bg-red-500 hover:bg-red-700 text-white font-bold py-2 w-1/3 m-auto my-5'
+    link.href= saveURL;
+    link.download = 'Inertia-QR Code';
+    link.innerHTML= 'Save Image';
+    document.getElementById('generated').appendChild(link);
 }
 
 hideSpinner()
